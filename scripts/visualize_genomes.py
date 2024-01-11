92360dea35d03ea3b84748cdc7fc92c2f6c749a5
Dany Doerr <daniel.doerr@hhu.de>
2024-01-11 12:42:10 +0100

    ignore telomere-telomere adjs (which are technical artefacts)

--- scripts/visualize_genomes.py
+++ scripts/visualize_genomes.py
@@ -10,6 +10,7 @@ locale.setlocale(locale.LC_ALL, 'en_US')
 if not os.environ.get('DISPLAY', None):
     import matplotlib; matplotlib.use('Agg')
 from matplotlib import pylab as plt
+from itertools import chain
 from collections import defaultdict
 import networkx as nx
 import csv
@@ -18,6 +19,7 @@ import csv
 import data_utils as du
 
 COLOR_MAP = {du.ETYPE_ADJ: 'black', du.ETYPE_ID: 'gray' }
+COLORS_COMP = ['orange', 'yellowgreen', 'lightseagreen', 'cornflowerblue', 'orchid']
 LINESTYLE_MAP = {du.ETYPE_ADJ: 'solid', du.ETYPE_ID: 'dotted' }
 
 
@@ -26,10 +28,12 @@ def constructGenomeGraph(adjacencies, genes):
 
     G = nx.MultiGraph()
 
-    for ext1, ext2 in adjacencies:
-        G.add_edge(ext1, ext2, type=du.ETYPE_ADJ)
+    for (g1, ext1), (g2, ext2) in adjacencies:
+        if ext1 != du.EXTR_CAP or ext2 != du.EXTR_CAP:
+            G.add_edge((g1, ext1), (g2, ext2), type=du.ETYPE_ADJ)
 
-    for gene in genes:
+    extremities = list(chain(*zip(*adjacencies)))
+    for gene in set(genes).difference(map(lambda x: x[0], filter(lambda x: x[1] == du.EXTR_CAP, extremities))):
         G.add_edge((gene, du.EXTR_HEAD), (gene, du.EXTR_TAIL),
                 type=du.ETYPE_ID)
     return G
@@ -42,6 +46,8 @@ if __name__ == '__main__':
             help='candidate adjacencies of the genomes in the phylogeny')
     parser.add_argument('-i', '--highlight', type=open,
             help='highlight adjacencies in visualization')
+    parser.add_argument('-o', '--omit_components', type=int, default=0,
+            help='Omit components of with x genes or less')
 
     args = parser.parse_args()
 
@@ -60,12 +66,21 @@ if __name__ == '__main__':
         # construct genome graph
         genes = candidateAdjacencies['genes'][species]
         G = constructGenomeGraph(adjacencies, genes)
-        H = nx.MultiGraph()
-        H.add_edges_from(G.edges)
+        k = 0
+        node_color = list()
+        nodes = list()
+        for c in sorted(nx.connected_components(G), key=len, reverse=True):
+            if len(c) <= args.omit_components * 2:
+                print(f'omitting component of size {len(c)//2} in plotting {species}', file=stderr)
+                G.remove_nodes_from(c)
+            else:
+                nodes.extend(c)
+                node_color.extend([COLORS_COMP[k % len(COLORS_COMP)]]* len(c))
+                k += 1
+
 
         # plot figure
         plt.subplot((n_species + 1) // 2, 2, i+1)
-#        layout = nx.spectral_layout(H)
         layout = nx.spring_layout(G, iterations=200)
 
         edges = list(G.edges(data = True))
@@ -83,10 +98,10 @@ if __name__ == '__main__':
                     edge_color = 'red', with_labels=False, node_size=0, \
                     width=4)
 
-        nx.draw_networkx(G, pos=layout, edgelist=edges, edge_color = \
-                edge_color, with_labels=False, font_size=1, \
-                node_color='orange', node_size=2, width=list(map(lambda x: x \
-                == 'black' and 2 or 1, edge_color)))
+        nx.draw_networkx(G, pos=layout, nodelist=nodes, node_color=node_color, \
+                         edgelist=edges, edge_color = edge_color, \
+                         with_labels=False, font_size=1, node_size=2, \
+                         width=list(map(lambda x: x == 'black' and 2 or 1, edge_color)))
 #        nx.draw_networkx(G, pos=layout, edgelist=edges, edge_color='black', \
 #                font_size=10, node_color='white', node_size=5, \
 #                width=list(map(lambda x: x == 'dotted' and 0.4 or 0.5, \
